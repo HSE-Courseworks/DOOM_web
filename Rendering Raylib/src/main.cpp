2a7a8dceb2f69fb69fb698098815533fa38ab764
DarrrNik <139496828+DarrrNik@users.noreply.github.com>
2024-06-11 13:35:11 +0300

    update function main

--- Rendering Raylib/src/main.cpp
+++ Rendering Raylib/src/main.cpp
@@ -1,8 +1,10 @@
 #include "raylib.h"
 #include "World.hpp"
 #include "Fps.hpp"
+#include "Client.hpp"
 #include <iostream>
 #include <cmath>
+#include <thread>
 
 int main()
 {
@@ -18,9 +20,9 @@ int main()
     Vector2 pos_1;
     pos_1.x = game.gameMap.getFrame().x + game.gameMap.getWallSize().x * 3 / 2.0f;
     pos_1.y = game.gameMap.getFrame().y + game.gameMap.getWallSize().y * 3 / 2.0f;
-    Vector2 pos_2;
-    pos_2.x = game.gameMap.getFrame().x + game.gameMap.getWallSize().x * (game.gameMap.getMazeSize().x - 3 / 2.0f);
-    pos_2.y = game.gameMap.getFrame().y + game.gameMap.getWallSize().y * (game.gameMap.getMazeSize().y - 3 / 2.0f);
+    //Vector2 pos_2;
+    //pos_2.x = game.gameMap.getFrame().x + game.gameMap.getWallSize().x * (game.gameMap.getMazeSize().x - 3 / 2.0f);
+    //pos_2.y = game.gameMap.getFrame().y + game.gameMap.getWallSize().y * (game.gameMap.getMazeSize().y - 3 / 2.0f);
     // Vector2 pos_3;
     // pos_3.x = game.gameMap.getFrame().x + game.gameMap.getWallSize().x * (game.gameMap.getMazeSize().x - 3 / 2.0f);
     // pos_3.y = game.gameMap.getFrame().y + game.gameMap.getWallSize().y * 3 / 2.0f;
@@ -29,7 +31,7 @@ int main()
     // pos_4.y = game.gameMap.getFrame().y + game.gameMap.getWallSize().y * (game.gameMap.getMazeSize().y - 3 / 2.0f);
 
     game.addPlayer(Player(sizePlayer, pos_1, 0, softRed, "resources/player_1.png"));
-    game.addPlayer(Player(sizePlayer, pos_2, 180, softBlue, "resources/player_2.png"));
+    //game.addPlayer(Player(sizePlayer, pos_2, 180, softBlue, "resources/player_2.png"));
     // game.addPlayer(Player(sizePlayer, pos_3, 180, softPink, "resources/player_3.png"));
     // game.addPlayer(Player(sizePlayer, pos_4, 0, softYellow, "resources/player_4.png"));
 
@@ -37,8 +39,16 @@ int main()
     music.looping = true;
     SetMusicVolume(music, 1);
     PlayMusicStream(music);
+    sf::TcpSocket TCPsock;
+    sf::UdpSocket UDPsock;
+    if (TCPsock.connect(ServIP, TCPPort) != sf::TcpSocket::Done)
+        TraceLog(LOG_ERROR, "Connection error. The client has not connected to a serv.");
+    UDPsock.bind(ClientUDPPort);
+    TCPsock.setBlocking(false);
+    //UDPsock.setBlocking(false);
     while (!WindowShouldClose())
     {
+        CheckNewData(TCPsock, UDPsock, game);
         UpdateMusicStream(music);
         BeginDrawing();
         ClearBackground(BLACK);
@@ -46,10 +56,14 @@ int main()
         game.showWorld();
         curFps.show();
         EndDrawing();
+        SendData(UDPsock);
     }
-    game.removePlayer(1); game.removePlayer(2); 
-    // game.removePlayer(3); // game.removePlayer(4);
+    sf::Packet pack;
+    pack << PLAYER_DISCONNECTED << game.GetCurPlayerID();
+    TCPsock.send(pack);
 
+    game.removePlayer(1); //game.removePlayer(2); 
+    // game.removePlayer(3); // game.removePlayer(4);
     UnloadMusicStream(music);
     CloseAudioDevice();
     CloseWindow();
