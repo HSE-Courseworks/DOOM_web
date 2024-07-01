0748d846164ba8dcc766651432abbccc53861247
DarrrNik <139496828+DarrrNik@users.noreply.github.com>
2024-07-01 09:29:19 +0300

    Update main.cpp

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -8,13 +8,14 @@
 
 int main(int argc, char* argv[]) {
     sf::TcpListener TCPListener;
-    TCPListener.listen(TCPPort, sf::IpAddress::getLocalAddress());
-    std::cout << sf::IpAddress::getLocalAddress();
+    TCPListener.listen(TCPPort);
+    //TCPListener.setBlocking(false);
+    std::cout << sf::IpAddress::getPublicAddress() << std::endl;
 
-    std::list<Lobby> lobbies;
-    std::list<std::thread> thrdsComputeLobbies;
+    //std::list<Lobby*> lobbies;
+    std::list<std::pair<Lobby*, std::thread>> listLobbiesThreads;
     
-    std::thread thrdCheckNew(CheckNewConnections, std::ref(TCPListener), std::ref(lobbies), std::ref(thrdsComputeLobbies));
+    std::thread thrdCheckNew(CheckNewConnections, std::ref(TCPListener), std::ref(listLobbiesThreads));
 
     thrdCheckNew.join();
     return 0;
